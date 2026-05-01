95c95f95174b93a5c8d0f63d7f3d26b59e14b03b
Muhammad Unais <unaismuhammas123@gmail.com>
2026-05-01 09:41:36 +0500

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -4,13 +4,28 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [inputValue, setInputValue] = useState("");
 
   const increment = () => setCount(count + 1);
   const decrement = () => setCount(count - 1);
  const reset = () => setCount(0);
+
+ // Form Submit Function
+
+ const handleSubmit = (e) => {
+  e.preventDefault();
+   setCount(Number(inputValue));
+   setInputValue("");
+ }
   return (
     <div className="app">
       <h1>React Counter App</h1>
+      {/* Form Handling */}
+      <form onSubmit={handleSubmit}>
+      <input type="number" placeholder="Enter Numer" value={inputValue}
+      onChange={(e) => setInputValue(e.target.value)}/>
+      <button type="submit">Set Counter</button>
+      </form>
 
       {/* Passing props to child */}
       <Counter 
